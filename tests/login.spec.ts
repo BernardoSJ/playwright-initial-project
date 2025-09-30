85b14415d65e193c7d70d93597b15e64926633a6
BernardoSJ <salinasjaquezb@gmail.com>
2025-09-30 13:56:28 -0600

    Add first login test and update npm scripts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-test.describe('Login', () => {
+test.describe('Login Module', () => {
+
+    test("Working with standard credentials", async ({page}) => {
+        const login = new LoginPage(page);
+        await login.goto();
+        await login.login('standard_user', 'secret_sauce');
+        await expect(page.locator('.title')).toHaveText('Products');
+    });
 
 });
